86cbfca407bc03c3ffd81e81969deaf25c9cf72e
Marbelis Poleo <poleomoncada@gmail.com>
2023-01-24 21:27:20 -0300

    card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardHoverEffeects1Component } from './components/card-hover-effeects1/card-hover-effeects1.component';
 import { LoginAnimadoComponent } from './components/login-animado/login-animado.component';
+import { Login2Component } from './components/login2/login2.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CardHoverEffeects1Component,
-    LoginAnimadoComponent
+    LoginAnimadoComponent,
+    Login2Component,
+
   ],
   imports: [
     BrowserModule,
